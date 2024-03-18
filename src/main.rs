4bff4732f41de5ca567e0ccc160c2790487175ab
bitdragon50 <boseme@hotmail.co.uk>
2024-03-18 06:33:06 +0000

    Shuffling the deck

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 use strum_macros::EnumIter;
 use strum::IntoEnumIterator;
+use std::convert::TryInto;
+use std::fmt::Debug;
+use rand::seq::SliceRandom; // Trait that provides the shuffle method
+use rand::thread_rng;       // Function that provides a random number generator
 
-#[derive(EnumIter,IntoEnumIterator)]
+
+fn vec_to_array<Card: std::fmt::Debug, const N: usize>(mut vec: Vec<Card>) -> Result<[Card; N], Vec<Card>> {
+    if vec.len() == N {
+        let array: [Card; N] = vec.drain(..).collect::<Vec<_>>().try_into().expect("Length mismatch");
+        Ok(array)
+    } else {
+        Err(vec)
+    }
+}
+
+
+
+#[derive(Debug,EnumIter,Clone)]
 enum Suit {
     Spade,
     Club,
@@ -9,6 +25,7 @@ enum Suit {
     Heart
 }
 
+#[derive(Debug,EnumIter)]
 enum Rank {
     Ace,
     Two,
@@ -25,6 +42,7 @@ enum Rank {
     King
 }
 
+#[derive(Debug)]
 struct Card {
     suit: Suit,
     rank: Rank
@@ -41,10 +59,13 @@ impl Card {
 //     suit: 
 // }
 
+#[derive(Debug)]
 struct Shuffled;
 
+#[derive(Debug)]
 struct Unshuffled;
 
+#[derive(Debug)]
 struct Deck<T> {
     state: T,
     cards: [Card; 52]
@@ -52,13 +73,31 @@ struct Deck<T> {
 
 impl Deck<Unshuffled> {
     fn new() -> Deck<Unshuffled> {
+        let mut vec_cards: Vec<Card> = vec![];
+        for suit in Suit::iter() {
+                for rank in Rank::iter() {
+                    vec_cards.push(Card { suit: suit.clone(), rank: rank })
+                }
+            }
+            
         Deck {
             state: Unshuffled,
-            cards: 
+            cards: vec_to_array(vec_cards).unwrap()
         }
+    }
+
+    fn shuffle_deck(mut self) -> Deck<Shuffled> {
+        let mut rng = thread_rng();
+        self.cards.shuffle(&mut rng);
+        Deck { state: Shuffled, cards: self.cards }
     }
 }
 
+
+
 fn main() {
-    println!("Hello, world!");
+    let playing_deck = Deck::new();
+    println!("{:#?}", &playing_deck);
+    
+    println!("{:#?}", playing_deck.shuffle_deck());
 }
